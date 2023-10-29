e5ef852e3d2c37e93b8cce7006cf5ee9cd1b4ec0
Muhammad Ribal <138850248+MuhammadRiba@users.noreply.github.com>
2023-10-29 13:51:52 +0500

    Add files via upload

---
+++ Ribal-Python-Class-2.py
@@ -0,0 +1,12 @@
+a = int(input("input your first Number:"))
+
+b = int(input("input your second number:"))
+
+c = input("input your objectives likes +,-,/,*")
+
+sum =  a + b
+gin =  a - b
+kin =  a / b
+jin =  a * b
+
+print("the sum of a and b is" , jin)
---
+++ Ribal-Python-Class-3.py
@@ -0,0 +1,7 @@
+num = 43124
+name = "Ribal"
+flo = 56.5632
+
+print(num,name,flo)
+
+
